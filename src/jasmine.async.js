da3d4d50bf1e6bbf36171db5ea7a44122c8cd58f
Fernando Hernandez <fernando@clarkparsia.com>
2013-06-07 15:35:59 -0400

    Added modularity for AMD/node.js

--- src/jasmine.async.js
+++ src/jasmine.async.js
@@ -1,52 +1,78 @@
-this.AsyncSpec = (function(global){
+// Copyright (c)2013 Muted Solutions, LLC. All Rights Reserved.
+// Distributed under MIT license
+// http://github.com/derickbailey/jasmine.async
+// fork from https://github.com/johntimothybailey/jasmine.async
 
-  // Private Methods
-  // ---------------
-  
-  function runAsync(block){
-    return function(){
-      var done = false;
-      var complete = function(){ done = true; };
+(function (root, factory) {
+    if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like enviroments that support module.exports,
+        // like Node.
+        module.exports = factory(root);
+    } else if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(function() {
+          return factory(root);
+        });
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory(root);
+  }
+}(this, function (root) {
 
-      runs(function(){
-        try{
-          block.call(this,complete);
-        } catch ( error ){
-          complete();
-          throw error;
-        }
-      });
+  var AsyncSpec = (function(global){
 
-      waitsFor(function(){
-        return done;
-      });
+    // Private Methods
+    // ---------------
+    
+    function runAsync(block, timeout){
+      return function(){
+        var done = false;
+        var complete = function(){ done = true; };
+
+        runs(function(){
+          try{
+            block.call(this,complete);
+          } catch ( error ){
+            complete();
+            throw error;
+          }
+        });
+
+        waitsFor(function(){
+          return done;
+        }, timeout);
+      };
+    }
+
+    // Constructor Function
+    // --------------------
+
+    function AsyncSpec(spec, timeout){
+      this.spec = spec;
+      this.timeout = timeout ? timeout : 5000;
+    }
+
+    // Public API
+    // ----------
+
+    AsyncSpec.prototype.beforeEach = function(block){
+      this.spec.beforeEach(runAsync(block, this.timeout));
     };
-  }
 
-  // Constructor Function
-  // --------------------
+    AsyncSpec.prototype.afterEach = function(block){
+      this.spec.afterEach(runAsync(block, this.timeout));
+    };
 
-  function AsyncSpec(spec){
-    this.spec = spec;
-  }
+    AsyncSpec.prototype.it = function(description, block){
+      // For some reason, `it` is not attached to the current
+      // test suite, so it has to be called from the global
+      // context.
+      global.it(description, runAsync(block, this.timeout));
+    };
 
-  // Public API
-  // ----------
-
-  AsyncSpec.prototype.beforeEach = function(block){
-    this.spec.beforeEach(runAsync(block));
-  };
-
-  AsyncSpec.prototype.afterEach = function(block){
-    this.spec.afterEach(runAsync(block));
-  };
-
-  AsyncSpec.prototype.it = function(description, block){
-    // For some reason, `it` is not attached to the current
-    // test suite, so it has to be called from the global
-    // context.
-    global.it(description, runAsync(block));
-  };
+    return AsyncSpec;
+  })(root);
 
   return AsyncSpec;
-})(this);
+}));
